28b6e89e2b72246beac3a9c33a7928eebd793b0e
Daniel <daylectro@yahoo.de>
2019-05-22 23:24:30 +0200

    Initial Commit

---
+++ PitchPerfect2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect2
+//
+//  Created by Daniel Tiemor on 22.05.19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
